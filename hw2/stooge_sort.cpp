75887bfae028b2b636c6a2d5608e4a7012efadb6
Jesse <househoj@oregonstate.edu>
2019-10-14 14:03:55 -0700

    formatted stooge_sort.cpp

--- hw2/stooge_sort.cpp
+++ hw2/stooge_sort.cpp
@@ -3,44 +3,41 @@
 
 void stooge_sort(int*, int, int);
 
-int main(int argc, char* argv[]){
-    int n = 0;
-    std::ifstream myfile;
-    myfile.open(argv[1]);
-    myfile >> n;
-    
-    int* arr = new int[n];
-    
-    for(int i = 0; i < n; i++){
-        myfile >> arr[i];
-    }
-    stooge_sort(arr, 0, n - 1);
-    
-    for(int i = 0; i < n; i++){
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-    myfile.close();
+int main(int argc, char* argv[]) {
+	int n = 0;
+	std::ifstream myfile;
+	myfile.open(argv[1]);
+	myfile >> n;
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+	    myfile >> arr[i];
+	}
+	stooge_sort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	myfile.close();
 	return 0;
 }
 
 void stooge_sort(int arr[], int first, int last) {
 	int temp = 0;
-	if (first >= last) {
-		return;
-	}
-
-	if (arr[first] > arr[last]) {
-		temp = arr[first];
-		arr[first] = arr[last];
-		arr[last] = temp;
-	}
-
-	if (last - first + 1 >= 3) {
-		int third = (last - first + 1) / 3;
-
-		stooge_sort(arr, first, last - third);
-		stooge_sort(arr, first + third, last);
-		stooge_sort(arr, first, last - third);
-	}
+    if(first >= last){
+        return;
+    }
+    if(arr[first] > arr[last]){
+        temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+    if(last - first + 1 >= 3){
+        int third = (last - first + 1) / 3;
+        stooge_sort(arr, first, last - third);
+        stooge_sort(arr, first + third, last);
+        stooge_sort(arr, first, last - third);
+    }
 }
